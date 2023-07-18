8d4868ee878658700e183698c40ca6ece71be9bc
Konstantin Glazunov <kglaz@ya.ru>
2023-07-18 17:09:14 +0200

    style

--- script.js
+++ script.js
@@ -8,14 +8,25 @@ function showImage() {
         data = response.json();
         data.then((res) => {
             const imgUrl = res.message;
-            if (!(document.getElementById('imageFromServer'))) { //не получилось выделить получение URL в отдельную функцию...
+           // if (!(document.getElementById('imageFromServer'))) { //не получилось выделить получение URL в отдельную функцию...
                 imageContent.insertAdjacentHTML('afterbegin', `<img id=imageFromServer src=${imgUrl} alt="imageFromServer">`)
                 const image = document.getElementById('imageFromServer');
                 setTimeout(() => image.remove(), 2000);
-            };
+        //    }
+
+
         })
     })
 }
 
 
-imageButton.addEventListener('click', showImage);
+imageButton.addEventListener('click', () => {
+    if (!document.getElementById('imageFromServer')) {
+        showImage();
+    } else {
+        (document.getElementById('imageFromServer')).remove();
+        showImage();
+
+    };
+}
+)
