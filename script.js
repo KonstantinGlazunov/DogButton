60b516fcf9ef067c380419560fda9a6087ec18bb
Konstantin Glazunov <kglaz@ya.ru>
2023-07-18 16:41:37 +0200

    initial

---
+++ script.js
@@ -0,0 +1,21 @@
+const imageButton = document.getElementById('button');
+const imageContent = document.getElementById('content');
+const serverURL = 'https://dog.ceo/api/breeds/image/random';
+
+function showImage() {
+    let response = fetch(serverURL);
+    response.then((response) => {
+        data = response.json();
+        data.then((res) => {
+            const imgUrl = res.message;
+            if (!(document.getElementById('imageFromServer'))) { //не получилось выделить получение URL в отдельную функцию...
+                imageContent.insertAdjacentHTML('afterbegin', `<img id=imageFromServer src=${imgUrl} alt="imageFromServer">`)
+                const image = document.getElementById('imageFromServer');
+                setTimeout(() => image.remove(), 2000);
+            };
+        })
+    })
+}
+
+
+imageButton.addEventListener('click', showImage);
